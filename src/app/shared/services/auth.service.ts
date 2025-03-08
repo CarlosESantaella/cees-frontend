0414ef05f0efedfea7cd1c6c2a75737edb585384
Carlos Santaella <carlos.santaella.cesg@gmail.com>
2025-03-08 00:52:51 -0500

    update to v18 core and cli

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,8 +1,4 @@
-import {
-  HttpClient,
-  HttpErrorResponse,
-  HttpHeaders,
-} from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
 import { catchError, finalize, map, of, switchMap, throwError } from 'rxjs';
